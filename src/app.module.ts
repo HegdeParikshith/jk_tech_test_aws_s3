e7e401fc7e9e38c18a2d678159f5fc8805ff8546
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 <parikshithhegde04@gmail.com>
2023-11-14 01:13:37 +0530

    changed database url to atlas

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BucketsModule } from './buckets/buckets.module';
 import { ObjectsModule } from './objects/objects.module';
+import { config } from 'dotenv';
 
+config();
 @Module({
   imports: [
     UsersModule,
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/s3'),
+    MongooseModule.forRoot(
+      'mongodb+srv://parikshithhegde04:NtDRW3EMCObHS4q3@cluster0.cvevuzs.mongodb.net/?retryWrites=true&w=majority',
+    ),
     BucketsModule,
     ObjectsModule,
   ],
